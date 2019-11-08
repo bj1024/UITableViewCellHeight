6b52b6a60c3d6d32a359411b74935a5038b4f34b
H.K <16552133+bj1024@users.noreply.github.com>
2019-11-08 18:47:03 +0900

    new project

---
+++ UITableViewCellHeight/UITableViewCellHeight/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2019, mycompany All rights reserved. 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
